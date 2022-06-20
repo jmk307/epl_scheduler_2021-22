09fb653f1f81a6aba08739b99f4fde81b6fe8b89
jmk307 <jimin112688@gmail.com>
2022-06-20 19:55:46 +0900

    feat : BoardService 추가

---
+++ src/main/java/com/football/epl_scheduler_202122/service/BoardService.java
@@ -0,0 +1,38 @@
+package com.football.epl_scheduler_202122.service;
+
+import com.football.epl_scheduler_202122.domain.Board;
+import com.football.epl_scheduler_202122.dto.Board.BoardRequestDTO;
+import com.football.epl_scheduler_202122.dto.Board.BoardResponseDTO;
+import com.football.epl_scheduler_202122.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public Long register(BoardRequestDTO boardRequestDTO) {
+        Board board = boardRequestDTO.toEntity();
+        boardRepository.save(board);
+        return board.getId();
+    }
+
+    public List<BoardResponseDTO> findBoards(String startDate) {
+        List<Board> boards = boardRepository.findAll(startDate);
+        return boards.stream().map(BoardResponseDTO::new).collect(Collectors.toList());
+    }
+
+    public BoardResponseDTO findBoard(Long id) {
+        Board board = boardRepository.findById(id).orElse(null);
+
+        return new BoardResponseDTO(board);
+    }
+
+    public void delete(Long id) {
+        boardRepository.deleteById(id);
+    }
+}
