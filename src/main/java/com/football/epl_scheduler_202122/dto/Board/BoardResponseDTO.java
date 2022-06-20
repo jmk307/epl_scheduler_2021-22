8c6890590863dbb21def0ae0f58d15e06c6c0379
jmk307 <jimin112688@gmail.com>
2022-06-20 19:55:03 +0900

    feat : BoardResponseDTO 추가

---
+++ src/main/java/com/football/epl_scheduler_202122/dto/Board/BoardResponseDTO.java
@@ -0,0 +1,36 @@
+package com.football.epl_scheduler_202122.dto.Board;
+
+import com.football.epl_scheduler_202122.domain.Board;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+@AllArgsConstructor
+@Builder
+public class BoardResponseDTO {
+    private Long id;
+
+    private String home;
+
+    private String away;
+
+    private String startDate;
+
+    private Date startTime;
+
+    private String result;
+
+    public BoardResponseDTO(Board board) {
+        this.id = board.getId();
+        this.home = board.getHome();
+        this.away = board.getAway();
+        this.startDate = board.getStartDate();
+        this.startTime = board.getStartTime();
+        this.result = board.getResult();
+    }
+}
