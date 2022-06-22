8e6ebb6467b70b7d7d5336c9b377199418272e60
jmk307 <jimin112688@gmail.com>
2022-06-23 05:02:26 +0900

    feat : 페이징 쿼리 추가

--- src/main/java/com/football/epl_scheduler_202122/repository/BoardRepository.java
+++ src/main/java/com/football/epl_scheduler_202122/repository/BoardRepository.java
@@ -1,6 +1,9 @@
 package com.football.epl_scheduler_202122.repository;
 
 import com.football.epl_scheduler_202122.domain.Board;
+import com.football.epl_scheduler_202122.dto.Board.BoardResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +13,6 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query("select b from Board b where b.startDate = :startDate")
     List<Board> findAll(@Param("startDate") String startDate);
+
+    Page<BoardResponseDTO> findByStartDate(String startDate, Pageable pageable);
 }
