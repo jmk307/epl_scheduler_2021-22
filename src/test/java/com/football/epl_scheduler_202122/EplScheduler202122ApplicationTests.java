80bd35a8507eb12222c1e473d23bca68eedf85c7
jmk307 <jimin112688@gmail.com>
2022-06-24 23:49:42 +0900

    feat : pageable 확인 테스트 코드

--- src/test/java/com/football/epl_scheduler_202122/EplScheduler202122ApplicationTests.java
+++ src/test/java/com/football/epl_scheduler_202122/EplScheduler202122ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.football.epl_scheduler_202122;
 
+import com.football.epl_scheduler_202122.dto.Board.BoardResponseDTO;
+import com.football.epl_scheduler_202122.repository.BoardRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 
 @SpringBootTest
 class EplScheduler202122ApplicationTests {
 
+    @Autowired
+    BoardRepository boardRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void paging() {
+
+        String startDate = "2021-08-14";
+
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        Page<BoardResponseDTO> result = boardRepository.findByStartDate(startDate, pageable);
+
+        System.out.println("----------------------");
+        System.out.println(result.getTotalPages());
+        System.out.println(result.getNumber());
+        System.out.println("----------------------");
+
     }
 
 }
