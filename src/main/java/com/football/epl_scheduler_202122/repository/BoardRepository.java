65202cc14c7d1494237a8f44a21992191e36dba8
jmk307 <jimin112688@gmail.com>
2022-07-03 03:16:17 +0900

    refactor : 단일 조건 검색 기능, 페이징 추가

--- src/main/java/com/football/epl_scheduler_202122/repository/BoardRepository.java
+++ src/main/java/com/football/epl_scheduler_202122/repository/BoardRepository.java
@@ -2,17 +2,15 @@ package com.football.epl_scheduler_202122.repository;
 
 import com.football.epl_scheduler_202122.domain.Board;
 import com.football.epl_scheduler_202122.dto.Board.BoardResponseDTO;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    @Query("select b from Board b where b.startDate = :startDate")
-    List<Board> findAll(@Param("startDate") String startDate);
+public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board> {
 
-    Page<BoardResponseDTO> findByStartDate(String startDate, Pageable pageable);
+    List<BoardResponseDTO> findAllByStartDateAndHomeContaining(@Param("startDate") String startDate, @Param("keyword") String keyword, Pageable pageable);
+
 }
