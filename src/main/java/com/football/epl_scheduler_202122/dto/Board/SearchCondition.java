5acc3aab361c1021551a39adc811c9397b6b149f
jmk307 <jimin112688@gmail.com>
2022-07-06 07:10:28 +0900

    feat : querydsl 코드 추가

---
+++ src/main/java/com/football/epl_scheduler_202122/dto/Board/SearchCondition.java
@@ -0,0 +1,19 @@
+package com.football.epl_scheduler_202122.dto.Board;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+public class SearchCondition {
+
+    private String content;
+
+    private SearchType type;
+
+    public SearchCondition(String content, SearchType type) {
+        this.content = content;
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/football/epl_scheduler_202122/dto/Board/SearchType.java
@@ -0,0 +1,5 @@
+package com.football.epl_scheduler_202122.dto.Board;
+
+public enum SearchType {
+    HOME, AWAY, HA
+}
