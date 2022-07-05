aae97522c15029e426e732a046031c7d7ae1234a
jmk307 <jimin112688@gmail.com>
2022-07-06 06:56:44 +0900

    refactor : querydsl에 맞게 리팩토링

--- src/main/java/com/football/epl_scheduler_202122/service/BoardService.java
+++ src/main/java/com/football/epl_scheduler_202122/service/BoardService.java
@@ -3,12 +3,14 @@ package com.football.epl_scheduler_202122.service;
 import com.football.epl_scheduler_202122.domain.Board;
 import com.football.epl_scheduler_202122.dto.Board.BoardRequestDTO;
 import com.football.epl_scheduler_202122.dto.Board.BoardResponseDTO;
+import com.football.epl_scheduler_202122.dto.Board.SearchCondition;
 import com.football.epl_scheduler_202122.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +24,9 @@ public class BoardService {
     }
 
     // 목록
-    public List<BoardResponseDTO> findBoards(String startDate, int page, Pageable pageable, String keyword) {
-        return boardRepository.findAllByStartDateAndHomeContaining(startDate, keyword, PageRequest.of(page, 3));
+    public List<BoardResponseDTO> findBoards(String startDate, int page, Pageable pageable, SearchCondition searchCondition) {
+        List<Board> search = boardRepository.search(searchCondition, startDate, PageRequest.of(page, 3));
+        return search.stream().map(BoardResponseDTO::new).collect(Collectors.toList());
     }
 
     public BoardResponseDTO findBoard(Long id) {
