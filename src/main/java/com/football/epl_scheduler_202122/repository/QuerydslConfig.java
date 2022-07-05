0fae646d2b09716ed5ad7805cf3c3eba849bf296
jmk307 <jimin112688@gmail.com>
2022-07-06 06:58:10 +0900

    feat : querydsl 설정 추가

---
+++ src/main/java/com/football/epl_scheduler_202122/repository/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package com.football.epl_scheduler_202122.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
